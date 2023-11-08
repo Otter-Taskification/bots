ebe3d9073295def416c942450a884d1c4c774cd6
Adam <73994535+adamtuft@users.noreply.github.com>
2023-11-08 10:40:24 +0000

    Add otter header

---
+++ common/otter.h
@@ -0,0 +1,10 @@
+#ifndef BOTS_OTTER_H
+#define BOTS_OTTER_H
+
+#if defined(WITH_OTTER)
+#include <otter/otter-task-graph-user.h>
+#else
+#include "otter-stub.h"
+#endif // WITH_OTTER
+
+#endif // BOTS_OTTER_H
