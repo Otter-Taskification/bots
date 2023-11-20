566800cef8fa80f59ecd17b2a622a7a06c93a024
Adam <73994535+adamtuft@users.noreply.github.com>
2023-11-20 16:52:52 +0000

    Add tied tasks version

--- omp-tasks/strassen/compile.sh
+++ omp-tasks/strassen/compile.sh
@@ -10,11 +10,11 @@ clang -fopenmp -O3   -o ../..//bin/strassen.clang.omp-tasks main.o strassen.o  .
 # clang -fopenmp -c -I../..//common -O3   -DIF_CUTOFF -I. -o main-if.o ../..//common/bots_main.c -DCDATE="\"2023/11/14;09:56\"" -DCC="\"clang -fopenmp\"" -DLD="\"clang -fopenmp\"" -DCMESSAGE="\"\"" -DLDFLAGS="\"-O3   \"" -DCFLAGS="\"-c -I../..//common -O3   -DIF_CUTOFF-I.\""
 # clang -fopenmp -c -I../..//common -O3   -DIF_CUTOFF -o strassen-if.o strassen.c
 # clang -fopenmp -O3   -o ../..//bin/strassen.clang.omp-tasks-if_clause main-if.o strassen-if.o  ../..//common/bots_common.o
-# clang -fopenmp -c -I../..//common -O3   -DFORCE_TIED_TASKS -I. -o main-tied.o ../..//common/bots_main.c -DCDATE="\"2023/11/14;09:56\"" -DCC="\"clang -fopenmp\"" -DLD="\"clang -fopenmp\"" -DCMESSAGE="\"\"" -DLDFLAGS="\"-O3   \"" -DCFLAGS="\"-c -I../..//common -O3   -DFORCE_TIED_TASKS -I.\""
-# cat strassen.c | sed -e "s/task \{1,\}untied/task/g" > tied-strassen.c ;\
-# clang -fopenmp -c -I../..//common -O3   -DFORCE_TIED_TASKS -o strassen-tied.o tied-strassen.c;\
-# rm tied-strassen.c
-# clang -fopenmp -O3   -o ../..//bin/strassen.clang.omp-tasks-tied main-tied.o strassen-tied.o  ../..//common/bots_common.o
+clang -fopenmp -DWITH_OTTER -c -I../..//common -O3   -DFORCE_TIED_TASKS -I. -o main-tied.o ../..//common/bots_main.c -DCDATE="\"2023/11/14;09:56\"" -DCC="\"clang -fopenmp\"" -DLD="\"clang -fopenmp\"" -DCMESSAGE="\"\"" -DLDFLAGS="\"-O3   \"" -DCFLAGS="\"-c -I../..//common -O3   -DFORCE_TIED_TASKS -I.\""
+cat strassen.c | sed -e "s/task \{1,\}untied/task/g" > tied-strassen.c ;\
+clang -fopenmp -DWITH_OTTER -c -I../..//common -O3   -DFORCE_TIED_TASKS -o strassen-tied.o tied-strassen.c;\
+rm tied-strassen.c
+clang -fopenmp -DWITH_OTTER -O3   -o ../..//bin/strassen.clang.omp-tasks-tied main-tied.o strassen-tied.o  ../..//common/bots_common.o -lotter-task-graph -lotf2 -lm -lstdc++
 # clang -fopenmp -c -I../..//common -O3   -DFORCE_TIED_TASKS -DMANUAL_CUTOFF -I. -o main-manual-tied.o ../..//common/bots_main.c -DCDATE="\"2023/11/14;09:56\"" -DCC="\"clang -fopenmp\"" -DLD="\"clang -fopenmp\"" -DCMESSAGE="\"\"" -DLDFLAGS="\"-O3   \"" -DCFLAGS="\"-c -I../..//common -O3   -DFORCE_TIED_TASKS -DMANUAL_CUTOFF -I.\""
 # cat strassen.c | sed -e "s/task \{1,\}untied/task/g" > tied-strassen.c ;\
 # clang -fopenmp -c -I../..//common -O3   -DMANUAL_CUTOFF -DFORCE_TIED_TASKS -o strassen-manual-tied.o strassen.c;\
